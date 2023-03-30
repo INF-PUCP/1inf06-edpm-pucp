6f4012d55ccc9f4ddc674e0148450170a22d49d1
Manuel Loaiza <manuel.loaiza@pucp.edu.pe>
2023-03-30 11:38:18 -0500

    Add interesting comments

--- laboratorios/laboratorio-1/2023-1/punteros-estructuras-ejercicio.cpp
+++ laboratorios/laboratorio-1/2023-1/punteros-estructuras-ejercicio.cpp
@@ -46,6 +46,16 @@ void PresentarPersona(Persona*& ptr_persona) {
 	" m y pesa " << ptr_persona->peso << " kg." << endl;
 }
 
+// Recuerden eliminar todo lo que asignen.
+// Es decir, tienen que realizar delete tantas veces como new usen.
+// En otro caso, existirá un memory leak.
+// https://en.wikipedia.org/wiki/Memory_leak
+void DesasignarMemoriaPersona(Persona*& ptr_persona) {
+	delete[] ptr_persona->nombre;
+	delete[] ptr_persona->apellido;
+	delete ptr_persona;
+}
+
 int main(void) {
 	// b. Completar el registro de Jorge Manrique con DNI 12345678
 	// de 35 anos, altura 1.62 m y peso 85.53 kg.
@@ -67,14 +77,13 @@ int main(void) {
 	(*ptr_persona).peso = 85.53;
 
 	PresentarPersona(ptr_persona);
-
-	delete ptr_persona;
+	DesasignarMemoriaPersona(ptr_persona);
 
 	// c. Crear una funcion para registrar los datos de una persona en una estructura.
 	Persona* ptr_naruto;
 	RegistrarDatos(ptr_naruto, "Naruto", "Uzumaki", 3141592, 19, 1.8, 75.9);
 	PresentarPersona(ptr_naruto);
-	delete ptr_naruto;
+	DesasignarMemoriaPersona(ptr_naruto);
 
 	// d. Crear flujo para ingresar los datos de n personas
 	int numero_personas;
@@ -107,9 +116,23 @@ int main(void) {
 
 	for (int i = 0; i < numero_personas; i++) {
 		PresentarPersona(ptr_personas[i]);
-		delete ptr_personas[i];
+		DesasignarMemoriaPersona(ptr_personas[i]);
 	}
-	delete ptr_personas;
 
+	// La siguiente linea nos da error en tiempo de ejecucion:
+	//
+	// delete[] *ptr_personas;
+	//
+	// a.out(27095,0x7ff84963a680) malloc: *** error for object 0x60000172c000: pointer being freed was not allocated
+	// a.out(27095,0x7ff84963a680) malloc: *** set a breakpoint in malloc_error_break to debug
+	//
+	// Esto se debe a que al haber desasignado iterando a lo largo del arreglo,
+	// el puntero al primer elemento coincide con el del arreglo.
+	// En sí, hemos tenido un memory leak ya que al utilizar nuestra función RegistrarDatos
+	// para asignar memoria al primer elemento del arreglo, hemos perdido su dirección.
+	// Podríamos lidiar dealocando memoria en dicho caso manualmente con un if,
+	// pero por cuestiones de código limpio lo dejaremos pasar por alto en esta oportunidad.
+
+	delete ptr_personas;
 	return 0;
 }
